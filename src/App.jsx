f8afb278331daa970a571959f1bb618eaa851c10
Mayur0018 <mayurnish18@gmail.com>
2025-05-28 14:47:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Box } from "@mui/material";
+import Navbar from "./Components/Navbar";
+import Feed from "./Components/Feed";
+import VideoDetail from "./Components/VideoDetail";
+import ChannelDetail from "./Components/ChannelDetail";
+import SearchFeed from "./Components/SearchFeed";
+import  SearchBar from "./Components/SearchBar";
+function App() {
+  return (
+    <BrowserRouter>
+
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Feed />}></Route>
+          <Route path="/video/:id" element={<VideoDetail />}></Route>
+          <Route path="/channel/:id" element={<ChannelDetail />}></Route>
+          <Route path="/search/:searchTerm" element={<SearchFeed />}></Route>
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { Paper, IconButton, colors } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+const SearchBar = () => {
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton>
+        <SearchIcon type="submit" sx={{ p: "10px", colors: "red" }} />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
